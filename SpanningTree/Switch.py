c3ee7a8b3f7b238d280d26ffdd421f40b21a87ea
mininet <tgycmzwj@gmail.com>
2022-05-20 17:32:56 -0400

    Finish Proj1

--- SpanningTree/Switch.py
+++ SpanningTree/Switch.py
@@ -33,18 +33,76 @@ class Switch(StpSwitch):
         # -self.switchID                   (the ID number of this switch object)
         # -self.links                      (the list of swtich IDs connected to this switch object)
         super(Switch, self).__init__(idNum, topolink, neighbors)
-
         # TODO: Define a data structure to keep track of which links are part of / not part of the spanning tree.
+        self.root = self.switchID
+        self.distance = 0
+        self.activeLinks = []
+        self.switchThrough = self.switchID
 
     def send_initial_messages(self):
         # TODO: This function needs to create and send the initial messages from this switch.
         #      Messages are sent via the superclass method send_message(Message msg) - see Message.py.
         #      Use self.send_message(msg) to send this.  DO NOT use self.topology.send_message(msg)
+        # need to send a message for each of the connected switches at initialization
+        for link in self.links:
+            #create the message object: claimedRoot, distanceToRoot, originID, destinationID, pathThrough
+            msg = Message(self.switchID, 0, self.switchID, link, False)
+            self.send_message(msg)
         return
 
     def process_message(self, message):
         # TODO: This function needs to accept an incoming message and process it accordingly.
         #      This function is called every time the switch receives a new message.
+        # add to active link
+        if message.pathThrough and message.origin not in self.activeLinks:
+            self.activeLinks.append(message.origin)
+        # remove from active link
+        if message.pathThrough==False and message.origin!=self.switchThrough and message.origin in self.activeLinks:
+            self.activeLinks.remove(message.origin)
+
+        # find a smaller root
+        if message.root<self.root:
+            # update root
+            self.root=message.root
+            # update distance
+            self.distance=message.distance+1
+            # update the switchThrough
+            self.switchThrough=message.origin
+            # update the activeLinks
+            if message.origin not in self.activeLinks:
+                self.activeLinks.append(message.origin)
+            # update neighbors with new message
+            for link in self.links:
+                msg=Message(self.root,self.distance,self.switchID,link,self.switchThrough==link)
+                self.send_message(msg)
+        #same root but shorter distance
+        if (message.root==self.root) and (message.distance+1<self.distance):
+            # remove old link from active links
+            self.activeLinks.remove(self.switchThrough)
+            # update distance
+            self.distance=message.distance+1
+            # update the switchThrough
+            self.switchThrough=message.origin
+            # update the activeLinks
+            if message.origin not in self.activeLinks:
+                self.activeLinks.append(message.origin)
+            # update neighbors with new message
+            for link in self.links:
+                msg=Message(self.root,self.distance,self.switchID,link,self.switchThrough==link)
+                self.send_message(msg)
+        #same root, same distance, but a smaller origin
+        if (message.root==self.root) and (message.distance+1==self.distance) and (message.origin<self.switchThrough):
+            # remove old link from active links
+            self.activeLinks.remove(self.switchThrough)
+            # update the switchThrough
+            self.switchThrough=message.origin
+            # update the activeLinks
+            if message.origin not in self.activeLinks:
+                self.activeLinks.append(message.origin)
+            # update neighbors with new message
+            for link in self.links:
+                msg=Message(self.root,self.distance,self.switchID,link,self.switchThrough==link)
+                self.send_message(msg)
         return
 
     def generate_logstring(self):
@@ -59,5 +117,9 @@ class Switch(StpSwitch):
         #      for switch 2 would have the following text:
         #      2 - 1, 2 - 3
         #      A full example of a valid output file is included (sample_output.txt) with the project skeleton.
-        return "switch log string, do not return a static string, build the log string"
+        all_active_links = sorted(self.activeLinks)
+        output_results=[]
+        for link in all_active_links:
+            output_results.append(str(self.switchID)+' - '+str(link))
+        return ', '.join(output_results)
 
