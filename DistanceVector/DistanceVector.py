3646ec142d9c765b7933060d1fcf97977d06c342
mininet <tgycmzwj@gmail.com>
2022-06-06 17:56:48 -0400

    project2 finished

--- DistanceVector/DistanceVector.py
+++ DistanceVector/DistanceVector.py
@@ -29,6 +29,7 @@ class DistanceVector(Node):
         super(DistanceVector, self).__init__(name, topolink, outgoing_links, incoming_links)
         
         # TODO: Create any necessary data structure(s) to contain the Node's internal state / distance vector data
+        self.dv={name:0}
 
     def send_initial_messages(self):
         """ This is run once at the beginning of the simulation, after all
@@ -41,6 +42,10 @@ class DistanceVector(Node):
 
         # TODO - Each node needs to build a message and send it to each of its neighbors
         # HINT: Take a look at the skeleton methods provided for you in Node.py
+        # msg[0]---origin
+        # msg[1]---dictionary of all topolinks (node, weight)
+        for last_neighbor in self.incoming_links:
+            self.send_msg([self.name,self.dv.copy()],last_neighbor.name)
 
     def process_BF(self):
         """ This is run continuously (repeatedly) during the simulation. DV
@@ -48,14 +53,45 @@ class DistanceVector(Node):
         messages that need to be sent to other nodes as a result are sent. """
 
         # Implement the Bellman-Ford algorithm here.  It must accomplish two tasks below:
-        # TODO 1. Process queued messages       
-        for msg in self.messages:            
-            pass
+        dv_before_update=self.dv.copy()
+        # TODO 1. Process queued messages
+        for msg in self.messages:
+            next_neighbor=msg[0]
+            for next_next_neighbor in msg[1].keys():
+                # discard the route to oneself
+                if next_next_neighbor==self.name:
+                    continue
+                # if the next_next_neighbor not yet discovered
+                elif next_next_neighbor not in self.dv.keys():
+                    if next_next_neighbor in [x.name for x in self.outgoing_links]:
+                        distance=int(self.get_outgoing_neighbor_weight(next_next_neighbor))
+                    else:
+                        distance=int(msg[1][next_next_neighbor])+int(self.get_outgoing_neighbor_weight(msg[0]))
+                    self.dv[next_next_neighbor]=distance
+                # if the next_next_neighbor already discovered
+                else:
+                    distance=int(msg[1][next_next_neighbor])+int(self.get_outgoing_neighbor_weight(msg[0]))
+                    # set to be -99 as long as one of the element is -99 (avoid situation -99+120=21 but -99 is actually -infinity)
+                    if int(msg[1][next_next_neighbor])<=-99 or int(self.get_outgoing_neighbor_weight(msg[0]))<=-99:
+                        self.dv[next_next_neighbor]=-99
+                    else:
+                        self.dv[next_next_neighbor]=max(min(self.dv[next_next_neighbor],distance),-99)
         
         # Empty queue
         self.messages = []
 
-        # TODO 2. Send neighbors updated distances               
+        # TODO 2. Send neighbors updated distances
+        # check whether is any update
+        update_flag=False
+        if len(dv_before_update)!=len(self.dv):
+            update_flag=True
+        else:
+            for key in self.dv.keys():
+                if self.dv[key]!=dv_before_update[key]:
+                    update_flag=True
+        if update_flag==True:
+            for last_neighbor in self.incoming_links:
+                self.send_msg([self.name,self.dv.copy()],last_neighbor.name)
 
     def log_distances(self):
         """ This function is called immedately after process_BF each round.  It 
@@ -70,4 +106,6 @@ class DistanceVector(Node):
         
         # TODO: Use the provided helper function add_entry() to accomplish this task (see helpers.py).
         # An example call that which prints the format example text above (hardcoded) is provided.        
-        add_entry("A", "A0,B1,C2")        
+        # add_entry("A", "A0,B1,C2")
+        results=[str(key)+str(val) for (key,val) in self.dv.items()]
+        add_entry(self.name,','.join(results))
